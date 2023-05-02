d7abbe5f54df676edbb6ab43e8f99baf775bcf49
Andrius Makutas <andrius.makutas@gmail.com>
2023-05-02 22:14:12 +0300

    added views/urls/templates for Car, 1 car, services, orders

---
+++ autoservice/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # INDEX
+    path('', views.index, name='index'),
+    # PAGE TO DISPLAY ALL CARS IN SERVICE
+    path('cars/', views.cars, name='cars'),
+    # DETAIL DISPLAY OF CAR
+    path('cars/<int:car_id>', views.specific_car, name='specific_car'),
+    # ALL SERVICES
+    path('services/', views.services, name='services'),
+    # ALL ORDERS
+    path('orders/', views.orders, name='orders'),
+    # SPECIFIC ORDER
+    path('orders/<int:order_id>/', views.specific_order, name='specific_order'),
+]
--- autoservice/views.py
+++ autoservice/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    car_models = CarModel.objects.all().values('brand')
+    car_model_count = CarModel.objects.count()
+    services = Service.objects.all().values('service_name')
+    services_count = Service.objects.count()
+
+    context = {
+        'car_models': car_models,
+        'car_model_count': car_model_count,
+        'services': services,
+        'services_count': services_count
+    }
+    return render(request, 'index.html', context)
+
+
+def cars(request):
+    all_cars = Car.objects.all()
+    context = {
+        'cars': all_cars
+    }
+    return render(request, "cars.html", context)
+
+
+def specific_car(request, car_id):
+    car = get_object_or_404(Car, pk=car_id)
+    context = {'car': car}
+    return render(request, "specific_car.html", context)
+
+
+def services(request):
+    all_services = Service.objects.all()
+    context = {
+        'services': all_services
+    }
+    return render(request, "services.html", context)
+
+
+def orders(request):
+    all_orders = OrderList.objects.all()
+    context = {
+        'orders': all_orders
+    }
+    return render(request, 'orders.html', context)
+
+
+def specific_order(request, order_id):
+    pass
+
